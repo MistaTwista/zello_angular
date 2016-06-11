1c4df26054840278dd30a973c56a18712034d0a4
Maxim Kernozhitskiy <maxim@aeromultimedia.com>
2016-06-12 02:22:04 +0300

    bower.son fix

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -27,11 +27,11 @@ module.exports = function(config) {
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-component-router/angular_1_router.js',
+      'bower_components/angular-component-router/ng_route_shim.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/Chart.js/dist/Chart.js',
-      'bower_components/angular-component-router/angular_1_router.js',
-      'bower_components/angular-component-router/ng_route_shim.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
