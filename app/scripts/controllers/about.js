d2f6a823268c6bd3de9ce05fb76148643f619180
Maxim Kernozhitskiy <maxim@aeromultimedia.com>
2016-06-11 12:01:28 +0300

    Initial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zelloApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zelloApp
+ */
+angular.module('zelloApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
